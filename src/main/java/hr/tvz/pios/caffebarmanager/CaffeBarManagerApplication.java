218bb9b1b8fe9bd10f0ddd4a987fed63aeea2afd
Marko Marijan <markomar543@gmail.com>
2024-03-04 22:20:00 +0100

    Initial commit

---
+++ src/main/java/hr/tvz/pios/caffebarmanager/CaffeBarManagerApplication.java
@@ -0,0 +1,13 @@
+package hr.tvz.pios.caffebarmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffeBarManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffeBarManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/tvz/pios/caffebarmanager/CaffeBarManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.pios.caffebarmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffeBarManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
